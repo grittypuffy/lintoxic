55041711086051e6f856f9b8c78b11d334212031
grittypuffy <grittypuffy@riseup.net>
2024-12-31 15:11:53 +0530

    Add: Toxic content prediction

--- lintoxic/utils/toxicity.py
+++ lintoxic/utils/toxicity.py
@@ -1,16 +1,40 @@
+import torch
+from detoxify import Detoxify
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-class TamilToxicityModel:
+class ToxicContentClassifier:
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        if ToxicContentClassifier._instance is None:
+            ToxicContentClassifier._instance = ToxicContentClassifier()
+        return ToxicContentClassifier._instance
+
+    def __init__(self):
+        if ToxicContentClassifier._instance is not None:
+            raise Exception("This is a singleton class, use the get_instance() method.")
+        
+        self.model = Detoxify("multilingual")
+
+    @torch.no_grad()
+    def predict(self, text: str):
+        prediction = self.model.predict(text)
+        offensive_entries = {key: value for key, value in prediction.items() if value > 0.8}
+        return offensive_entries if offensive_entries else prediction
+
+
+class TamilToxicContentClassifier:
     _instance = None
 
     @staticmethod
     def get_instance(model_name="Hate-speech-CNERG/deoffxlmr-mono-tamil"):
-        if ModelSingleton._instance is None:
-            ModelSingleton._instance = ModelSingleton(model_name)
-        return ModelSingleton._instance
+        if TamilToxicContentClassifier._instance is None:
+            TamilToxicContentClassifier._instance = TamilToxicContentClassifier(model_name)
+        return TamilToxicContentClassifier._instance
 
     def __init__(self, model_name):
-        if ModelSingleton._instance is not None:
+        if TamilToxicContentClassifier._instance is not None:
             raise Exception("This is a singleton class, use the get_instance() method.")
         
         self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
